45e8d669a76e5a10e942f6927dadde1d22a39ee8
Montoya-08 <gelicamontoyt@gmail.com>
2025-12-08 17:19:38 -0500

    Correción último commit

--- Controllers/service.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PracticaFinal.Controllers
-{
-    internal class Service
-    {
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public bool Status { get; set; }
-    }
-}
